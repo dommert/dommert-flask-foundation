330cb5dc12f44036ab74e3a6ccaf4eae3b51be20
Dommert <DommertEnterprises@Gmail.com>
2014-06-10 06:14:25 -0700

    Flask Foundation Template
    Base Flask Template with Zurb Foundation
    Includes JQuery

---
+++ flaskapp/main.py
@@ -0,0 +1,16 @@
+# Dommert's Flask Foundation
+# Dommert@Gmail.com (GitHub.com/Dommert)
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world(title='Hello World'):
+    return render_template('foundation/default.html', title=title)
+
+@app.route('/about')
+def about(title='About Page'):
+    return render_template('foundation/onecol.html', title=title)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
